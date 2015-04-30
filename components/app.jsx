fc2af79f1b15475dc79d78da43a09f7d2949e195
Marco Bettiolo <marco@bettiolo.it>
2015-04-30 12:15:27 +0100

    Browser based rendering of React classes

---
+++ components/app.jsx
@@ -0,0 +1,4 @@
+//var React = require('react');
+// var Hello = require('./Hello.jsx');
+
+React.render(<Hello />, document.getElementById('app'));
--- components/hello.jsx
+++ components/hello.jsx
@@ -1,4 +1,9 @@
-React.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
-);
+window.Hello = React.createClass({
+  render : function() {
+    return(
+      <h1>Hello, world!</h1>
+    );
+  }
+});
+
+// module.exports = Hello;
