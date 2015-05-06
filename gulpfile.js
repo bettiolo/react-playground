bfdd46052cfbbe6f0bf600aed3f9563cb6765ee5
Marco Bettiolo <marco@bettiolo.it>
2015-05-06 19:26:51 +0100

    separated browserify and minify and added clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 /*eslint-disable no-console, strict */
 'use strict';
 let gulp = require('gulp');
+let rename = require('gulp-rename');
+let del = require('del');
 let browserify = require('browserify');
 let babelify = require('babelify');
 let source = require('vinyl-source-stream');
@@ -8,21 +10,32 @@ let buffer = require('vinyl-buffer');
 let sourcemaps = require('gulp-sourcemaps');
 let uglify = require('gulp-uglify');
 
-gulp.task('browserify', () => {
+const reactApp = './components/app.jsx';
+const destination = './dist';
+
+gulp.task('clean', (cb) => {
+  del([ 'dist/'], cb);
+});
+
+gulp.task('browserify', ['clean'], () => {
   let bundler = browserify({
     debug: true, // append source maps
     transform: [ babelify ] // transpile es6/jsx to es5
   });
   return bundler
-    .require('./components/app.jsx', { entry: true }) // React app's entry point
+    .require(reactApp, { entry: true }) // React app's entry point
     .bundle() // generates a single stream with inline source maps
     .pipe(source('bundle.js')) // bundled output file name
-    .pipe(buffer()) // convert streaming vinyl files to use buffers
-    .pipe(sourcemaps.init({ loadMaps: true })) // load existing source maps
-      .pipe(uglify()) // minimise bundled file
-      .on('error', console.error.bind(console)) // write errors to console
-    .pipe(sourcemaps.write('./')) // create source maps
-    .pipe(gulp.dest('./dist')); // write minimised file and source maps to disk
+    .pipe(gulp.dest(destination)); // write minimised file and source maps to disk
 });
 
-gulp.task('build', ['browserify']);
+gulp.task('minify', ['browserify'], () => {
+  gulp.src('./dist/bundle.js')
+    .pipe(sourcemaps.init({ loadMaps: true })) // load existing source maps
+      .pipe(uglify()) // minimise bundled file
+      .pipe(rename({ extname: '.min.js' }))
+    .pipe(sourcemaps.write('./')) // create source maps
+    .pipe(gulp.dest(destination)); // write minimised file and source maps to disk
+});
+
+gulp.task('build', ['browserify', 'minify']);
