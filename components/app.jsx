ac39e72f45d73967ada6377321a02472da658e5a
Marco Bettiolo <marco@bettiolo.it>
2015-05-07 00:21:19 +0100

    making eslint happy

--- components/app.jsx
+++ components/app.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
-import Hello from './Hello.jsx'; //eslint-disable-line no-unused-vars
+import Hello from './Hello.jsx'; // eslint-disable-line no-unused-vars
 
 React.render(<Hello />, document.getElementById('app'));
