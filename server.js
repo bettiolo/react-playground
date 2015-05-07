4ebdfd0d6d21fcb7b8630c3989f4c72a0542eabe
Marco Bettiolo <marco@bettiolo.it>
2015-05-07 01:12:12 +0100

    Debugging when event loop is blocked

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 let log = require('debug')('app');
+let warn = require('debug')('app:warning');
 let path = require('path');
+let blocked = require('blocked');
 let express = require('express');
 let app = express();
+
+blocked((ms) => warn('Event loop blocked for %sms', ms | 0));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('views', path.join(__dirname, 'views'));
