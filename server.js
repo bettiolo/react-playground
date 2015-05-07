0fa4150b77a881b0db7cb68357433ba43a7d870d
Marco Bettiolo <marco@bettiolo.it>
2015-05-07 01:24:26 +0100

    Added some more logging

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 let log = require('debug')('app');
 let warn = require('debug')('app:warning');
+
+log('Initialising...');
+require('blocked')((ms) => warn('Event loop blocked for %sms', ms | 0));
 let path = require('path');
-let blocked = require('blocked');
 let express = require('express');
+
+log('Configuring Express...');
 let app = express();
-
-blocked((ms) => warn('Event loop blocked for %sms', ms | 0));
-
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
 
+log('Registering babel transpiler...');
 require('babel/register');
+
+log('Loading React router...');
 require('./src/react-router')(app);
 
-app.listen(app.get('port'), () => log('Server listening on port ' + app.get('port')));
+log('Starting express...');
+app.listen(app.get('port'), () => log('Express listening on port ' + app.get('port')));
